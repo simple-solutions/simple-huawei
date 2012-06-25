f562d3d586d8356066afe290f13859d8f72abe16
Dan Prince <danielprince@hotmail.co.uk>
2012-06-25 12:09:10 +0100

    Structure and templates created.

---
+++ src/controller/Application.java
@@ -0,0 +1,10 @@
+package controller;
+/**
+ * Application.java - Provide interaction between the model and the view.
+ * @author simple-developer
+ * This is the main class that will control the other aspects of application.
+ */
+
+public class Application {
+
+}
